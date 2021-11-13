2addc8e09a92884128726f3af0b013e6e010a350
ababu <ababualemu123@gmail.com>
2021-11-13 11:31:20 +0300

    forms added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+
+import React,{ Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <section className='section_1'>
+          <h4>Personal Info</h4>
+          <form>
+            <label htmlFor='name'>Name</label>
+            <input id='name' type='text'/>
+            <br></br>
+            <label htmlFor='email'>Email</label>
+            <input id='email' type='email'/>
+            <br></br>
+            <label htmlFor='phone'>Phone number</label>
+            <input id='phone' type='tel'/>
+          </form>
+        </section>
+        <section className='section_2'>
+          <h4>Educational Background</h4>
+          <form>
+            <label htmlFor='school'>Scool</label>
+            <input id='school' type='text'/>
+            <br></br>
+            <label htmlFor='study'>Study</label>
+            <input id='study' type='text'/>
+            <br></br>
+            <label htmlFor='form'>From</label>
+            <input id='from' type='date'/>
+            <label htmlFor='to'>To</label>
+            <input id='to' type='date'/>
+          </form>
+        </section>
+        <section className='section_3'>
+          <h4>Work Expirience</h4>
+          <form>
+            <label htmlFor='company'>Company</label>
+            <input id='compaby' type='text'/>
+            <br></br>
+            <label htmlFor='role'>Role</label>
+            <input id='role' type='text'/>
+            <br></br>
+            <label htmlFor='form'>From</label>
+            <input id='from' type='date'/>
+            <label htmlFor='to'>To</label>
+            <input id='to' type='date'/>
+          </form>
+        </section>
+      </div>
+    )
+  }
 }
 
 export default App;
