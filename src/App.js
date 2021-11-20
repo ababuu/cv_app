105ae5adf67bde6fa90c75627c8367ad9d2a9778
ababu <ababualemu123@gmail.com>
2021-11-20 11:01:08 +0300

    changes to functional component using hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 
-import React,{ Component } from 'react';
+import React,{useState } from 'react';
 import './App.css';
 import Display from './components/display';
 import { scroller } from "react-scroll";
 
-class App extends Component{
-  constructor(props){
-    super(props)
-    this.state={
+function App() {
+  const[inputs,setInputs]=useState(
+    {
       name: '',
       title:'',
       email: '',
@@ -29,17 +28,15 @@ class App extends Component{
       work_from:'',
       work_to:'',
     }
-    this.handleInputChange=this.handleInputChange.bind(this);
-    this.scrollToSection=this.scrollToSection.bind(this);
-  }
-  handleInputChange(e){
+  );
+  const handleInputChange=(e)=>{
       const {name,value}=e.target;
-      this.setState({
+      setInputs({
         ...value,
-        [name]: value
+        [inputs]: value
       })
   }
-  scrollToSection = (e) => {
+  const scrollToSection = (e) => {
     e.preventDefault();
     scroller.scrollTo("preview_section", {
       duration: 800,
@@ -47,7 +44,6 @@ class App extends Component{
       smooth: "easeInOutQuart",
     });
   };
-  render(){
     return(
       <div className='main_container'>
         <nav><h1>CV MAKER</h1></nav>
@@ -55,37 +51,37 @@ class App extends Component{
           <form>
           <h4>Personal Info</h4>
             <input id='name' type='text' name='name' placeholder="Name" onChange={this.handleInputChange}/>
-            <input id='title' type='text' name='title' placeholder="Professional Title" onChange={this.handleInputChange}/>
+            <input id='title' type='text' name='title' placeholder="Professional Title" onChange={handleInputChange}/>
             <br></br>
-            <input id='email' type='email' name='email' placeholder="Email" onChange={this.handleInputChange}/>
+            <input id='email' type='email' name='email' placeholder="Email" onChange={handleInputChange}/>
             <br></br>
-            <input id='phone' type='tel' name='phone' placeholder="Phone Number" onChange={this.handleInputChange}/>
-            <input id='title' type='text' name='adress' placeholder="Adress" onChange={this.handleInputChange}/>
-            <textarea type='text' name='about_me' placeholder="About me" onChange={this.handleInputChange} rows="8"/>
+            <input id='phone' type='tel' name='phone' placeholder="Phone Number" onChange={handleInputChange}/>
+            <input id='title' type='text' name='adress' placeholder="Adress" onChange={handleInputChange}/>
+            <textarea type='text' name='about_me' placeholder="About me" onChange={handleInputChange} rows="8"/>
           <h4>Educational Background</h4>
-          <input type='text' name='degree' placeholder="Degree Bsc, Masters or Phd" onChange={this.handleInputChange}/>
+          <input type='text' name='degree' placeholder="Degree Bsc, Masters or Phd" onChange={handleInputChange}/>
             <br></br>
-            <input id='study' type='text' name='study' placeholder="Field of Study" onChange={this.handleInputChange}/>
+            <input id='study' type='text' name='study' placeholder="Field of Study" onChange={handleInputChange}/>
             <br></br>
-            <input id='school' type='text' name='school' placeholder="School Name" onChange={this.handleInputChange}/>
+            <input id='school' type='text' name='school' placeholder="School Name" onChange={handleInputChange}/>
             <br></br>
-            <input type='number' name='school_from' placeholder="From" onChange={this.handleInputChange}/>
-            <input type='number' name='school_to' placeholder="To" onChange={this.handleInputChange}/>
+            <input type='number' name='school_from' placeholder="From" onChange={handleInputChange}/>
+            <input type='number' name='school_to' placeholder="To" onChange={handleInputChange}/>
           <h4>Work Expirience</h4>
-            <input id='company' type='text' name='company' placeholder="Company Name" onChange={this.handleInputChange}/>
+            <input id='company' type='text' name='company' placeholder="Company Name" onChange={handleInputChange}/>
             <br></br>
             <input type='text' name='role' placeholder="Role" onChange={this.handleInputChange}/>
-            <input type='number' name='work_from' placeholder="From (starting-year)" onChange={this.handleInputChange}/>
-            <input type='number' name='work_to' placeholder="To (final-year)" onChange={this.handleInputChange}/>
+            <input type='number' name='work_from' placeholder="From (starting-year)" onChange={handleInputChange}/>
+            <input type='number' name='work_to' placeholder="To (final-year)" onChange={handleInputChange}/>
             <br></br>
             <textarea placeholder="Role Description" name="Message" rows="10"/>
             <br></br>
             <h4>Skills</h4>
-            <input type='text' name='skill_1' placeholder="skill 1" onChange={this.handleInputChange}/>
-            <input type='text' name='skill_2' placeholder="skill 2" onChange={this.handleInputChange}/>
-            <input type='text' name='skill_3' placeholder="skill 3" onChange={this.handleInputChange}/>
-            <input type='text' name='skill_4' placeholder="skill 4" onChange={this.handleInputChange}/>
-            <button className='submit_btn' onClick={this.scrollToSection}>Preview CV</button>
+            <input type='text' name='skill_1' placeholder="skill 1" onChange={handleInputChange}/>
+            <input type='text' name='skill_2' placeholder="skill 2" onChange={handleInputChange}/>
+            <input type='text' name='skill_3' placeholder="skill 3" onChange={handleInputChange}/>
+            <input type='text' name='skill_4' placeholder="skill 4" onChange={handleInputChange}/>
+            <button className='submit_btn' onClick={scrollToSection}>Preview CV</button>
             <br></br>
             <button className='reset_btn' >Reset</button>
             <br></br>
@@ -97,6 +93,6 @@ class App extends Component{
       </div>
     )
   }
-}
+
 
 export default App;
